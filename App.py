b2bab2ab37a7fce48672003a50729185c4e5098a
tomeklarysz <ttomek.larysz@gmail.com>
2025-04-12 13:40:09 +0200

    add frame that holds interaction section, delete node possible

--- App.py
+++ App.py
@@ -12,17 +12,38 @@ class App:
         graph_frame = tk.Frame(master)
         graph_frame.pack()
 
+        form_frame = tk.Frame(master)
+        form_frame.pack()
+
         self.draw = DrawingGraph(self.graph, graph_frame)
 
-        self.entry = tk.Entry(master)            # For node name
-        self.entry.pack()
-        self.edges_entry = tk.Entry(master)       # For comma-separated connections
-        self.edges_entry.pack()
 
-        add_btn = tk.Button(master, text="Add Node", command=self.add_vertex)
-        add_btn.pack()
-        del_btn = tk.Button(master, text="Delete Node", command=self.del_vertex)
-        del_btn.pack()
+        tk.Label(form_frame, text="Dodaj wierzchołek:").grid(row=0, column=0, sticky='w')
+        self.vertex_entry = tk.Entry(form_frame)
+        self.vertex_entry.grid(row=0, column=1)
+
+        tk.Label(form_frame, text="Połączony z:").grid(row=1, column=0, sticky='w')
+        self.edges_entry = tk.Entry(form_frame)
+        self.edges_entry.grid(row=1, column=1)
+
+        add_btn = tk.Button(form_frame, text="Dodaj", command=self.add_vertex)
+        add_btn.grid(row=2, column=0)
+        
+        tk.Label(form_frame, text="Usuń wierzhołek:").grid(row=0, column=2, sticky='w')
+        self.delete_entry = tk.Entry(form_frame)
+        self.delete_entry.grid(row=0, column=3)
+        
+        del_btn = tk.Button(form_frame, text="Usuń", command=self.del_vertex)
+        del_btn.grid(row=1, column=2)
+
+        tk.Label(form_frame, text="Najkrótsza ścieżka").grid(row=0, column=4)
+        tk.Label(form_frame, text="Początek:").grid(row=1, column=4, sticky='w')
+        self.start_entry = tk.Entry(form_frame)
+        self.start_entry.grid(row=1, column=5)
+
+        tk.Label(form_frame, text="Koniec:").grid(row=2, column=4, sticky='w')
+        self.end_entry = tk.Entry(form_frame)
+        self.end_entry.grid(row=2, column=5)
 
         self.displayGraph()
 
@@ -32,7 +53,10 @@ class App:
 
     
     def add_vertex(self):
-        node = self.entry.get().strip()
+        if len(self.vertex_entry.get().split(',')) > 1:
+            print('Wprowadź tylko jeden wierzchołek')
+            return
+        node = self.vertex_entry.get().strip()
         edges = []
         if self.edges_entry.get():
             edges = self.edges_entry.get().split(',')
@@ -44,7 +68,9 @@ class App:
                 print('Niepoprawnie wprowadzone wierzchołki')
                 return
             if edge not in self.graph.adjacency_list:
-                self.graph.adjacency_list[edge] = []
+                # self.graph.adjacency_list[edge] = []
+                print('Co najmniej jeden z wierzchołków nie jest stworzony')
+                return
         
         for edge in edges:
             self.graph.add_edge(node, edge, 1)
@@ -52,5 +78,8 @@ class App:
         self.displayGraph()
     
     def del_vertex(self):
-        print('papapaf')
+        nodes_to_delete = self.delete_entry.get().split(',')
+        for node in nodes_to_delete:
+            self.graph.delete_vertex(node)
         
+        self.displayGraph()
--- DrawingGraph.py
+++ DrawingGraph.py
@@ -9,7 +9,7 @@ class DrawingGraph:
         self.graph = graph
         # self.pos = {}
 
-        self.figure = Figure(figsize=(5, 5))
+        self.figure = Figure(figsize=(10, 5))
         self.ax = self.figure.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.figure, master=parent_frame)
         self.canvas.get_tk_widget().pack()
@@ -18,17 +18,28 @@ class DrawingGraph:
         self.ax.clear()
         G = nx.Graph()
         edges = self.graph.get_edges()
+        vertices = self.graph.get_vertices()
 
         for i in edges:
             G.add_nodes_from(i[0])
             G.add_edges_from([(i[0], i[1])])
             G.add_edge(i[0], i[1], weight=i[2])
-        
-        # fixed_nodes = list(self.pos.keys())
 
-        # pos = nx.spring_layout(G, pos=fixed_nodes, fixed=fixed_nodes, seed=42)
-        # nx.draw(G, pos, with_labels=True, ax=self.ax, node_color='skyblue',
+        for i in vertices:
+            G.add_node(i)
+        
+        # if not self.pos:
+            # self.pos = nx.spring_layout(G, seed=42)
+            # self.pos = nx.spectral_layout(G)
+        # else:
+        #     fixed_nodes = list(self.pos.keys())
+        #     # self.pos = nx.spring_layout(G, pos=self.pos, fixed=fixed_nodes, seed=42)
+        #     self.pos = nx.spectral_layout(G, pos=self.pos, fixed=fixed_nodes, seed=42)
+
+        # nx.draw(G, pos=self.pos, with_labels=True, ax=self.ax, node_color='skyblue',
         #         edge_color='gray', node_size=1500, font_size=16)
+
+
         nx.draw(G, with_labels=True, ax=self.ax, node_color='skyblue',
                  edge_color='gray', node_size=1500, font_size=16)
         self.canvas.draw()
