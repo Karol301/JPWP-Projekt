ef3f9d09956b8fd84a4f9d6a0e8a87470ba08b03
tomeklarysz <ttomek.larysz@gmail.com>
2025-04-14 23:36:19 +0200

    wyswietlanie wagi krawedzi

--- DrawingGraph.py
+++ DrawingGraph.py
@@ -1,5 +1,4 @@
 import networkx as nx
-# import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # this is matplotlib embedded in Tkinter
@@ -7,7 +6,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 class DrawingGraph:
     def __init__(self, graph, parent_frame):
         self.graph = graph
-        # self.pos = {}
+        self.pos = {}
 
         self.figure = Figure(figsize=(10, 5))
         self.ax = self.figure.add_subplot(111)
@@ -15,31 +14,53 @@ class DrawingGraph:
         self.canvas.get_tk_widget().pack()
     
     def draw(self):
-        self.ax.clear()
-        G = nx.Graph()
-        edges = self.graph.get_edges()
-        vertices = self.graph.get_vertices()
-
-        for i in edges:
-            G.add_nodes_from(i[0])
-            G.add_edges_from([(i[0], i[1])])
-            G.add_edge(i[0], i[1], weight=i[2])
-
-        for i in vertices:
-            G.add_node(i)
         
-        # if not self.pos:
-            # self.pos = nx.spring_layout(G, seed=42)
-            # self.pos = nx.spectral_layout(G)
-        # else:
-        #     fixed_nodes = list(self.pos.keys())
-        #     # self.pos = nx.spring_layout(G, pos=self.pos, fixed=fixed_nodes, seed=42)
-        #     self.pos = nx.spectral_layout(G, pos=self.pos, fixed=fixed_nodes, seed=42)
+        self.ax.clear()
+    
+        G = nx.Graph()
 
-        # nx.draw(G, pos=self.pos, with_labels=True, ax=self.ax, node_color='skyblue',
-        #         edge_color='gray', node_size=1500, font_size=16)
+        for node in self.graph.get_vertices():
+            G.add_node(node)
 
-
-        nx.draw(G, with_labels=True, ax=self.ax, node_color='skyblue',
-                 edge_color='gray', node_size=1500, font_size=16)
+        for u, v, w in self.graph.get_edges():
+            G.add_edge(u, v, weight=w)
+    
+        self.pos = {node: pos for node, pos in self.pos.items() if node in G.nodes}
+        if self.pos:
+            # Keep old nodes fixed, layout new ones
+            fixed_nodes = list(self.pos.keys())
+            self.pos = nx.spring_layout(G, pos=self.pos, fixed=fixed_nodes, seed=42)
+        else:
+            # First time drawing — calculate all positions
+            self.pos = nx.spring_layout(G, seed=42)
+    
+        nx.draw(
+            G,
+            pos=self.pos,
+            with_labels=True,
+            ax=self.ax,
+            node_color='skyblue',
+            edge_color='gray',
+            node_size=1500,
+            font_size=16
+        )
+    
+        edge_labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(
+            G,
+            pos=self.pos,
+            edge_labels=edge_labels,
+            ax=self.ax,
+            label_pos=0.6,
+            font_size=12,
+            font_color='black',
+            rotate=False
+        )
+    
         self.canvas.draw()
+
+        # todo:
+        # dodawanie wagi przy nowych krawedziach
+        # zmienic sposob dodawania wierzcholkow
+        # przy dodawaniu paru wierzcholkow po przecinku tworzy sie ich za duzo
+        # po kliknieciu obliczanie dijkstry
